90a39d173fe2a166fff72c4802c73c9a4e945d56
Gatere Kinyanjui <gaterekinyanjui@gmail.com>
2024-12-28 17:45:38 +0300

    refactor: seed db with prisma seeding

--- db-services/prismaClientInstance.ts
+++ db-services/prismaClientInstance.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
 // https://www.prisma.io/docs/orm/prisma-client/setup-and-configuration/databases-connections
-export const prismaClientInstance = new PrismaClient({ log: ["query"] });
+export const prismaClientInstance = new PrismaClient();
+// { log: ["query"] }
